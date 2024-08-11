4841255a43933c3c15f660edf0f9071f63ba9010
Manish sonawane <manishsonawane3010@gmail.com>
2024-08-11 22:00:05 +0530

    TempCast

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import WeatherCard from './components/WeatherCard';
+import './App.css';
+import './components/WeatherCard.css';
+
+function App() {
+  return (
+    <div className="App">
+      <WeatherCard />
+    </div>
+  );
+}
+
+export default App;
